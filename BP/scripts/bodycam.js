a0ee2f953395f580d9f287de505fed0d4f3cf29d
Godruki <joaodinucci2010@gmail.com>
2026-07-22 04:34:53 -0300

    Update bodycam.js

--- BP/scripts/bodycam.js
+++ BP/scripts/bodycam.js
@@ -1,27 +1,57 @@
-const enabledPlayers = new Map();
+import { system } from "@minecraft/server";
+
+const cameras = new Map();
 
 export function toggleBodyCam(player) {
 
     const id = player.id;
 
-    if (enabledPlayers.has(id)) {
+    if (cameras.has(id)) {
 
-        enabledPlayers.delete(id);
+        cameras.delete(id);
 
-        player.sendMessage("§cBodyCam OFF");
+        player.sendMessage("§c📹 BodyCam apagada");
 
-    } else {
-
-        enabledPlayers.set(id, true);
-
-        player.sendMessage("§aBodyCam ON");
+        return;
 
     }
 
+    cameras.set(id, {
+
+        recording: true,
+
+        battery: 100,
+
+        started: Date.now()
+
+    });
+
+    player.sendMessage("§a📹 BodyCam encendida");
+
 }
 
-export function isBodyCamEnabled(player) {
+export function getCamera(player) {
 
-    return enabledPlayers.has(player.id);
+    return cameras.get(player.id);
 
 }
+
+export function isRecording(player) {
+
+    return cameras.has(player.id);
+
+}
+
+system.runInterval(() => {
+
+    for (const [, camera] of cameras) {
+
+        if (camera.battery > 0) {
+
+            camera.battery -= 0.02;
+
+        }
+
+    }
+
+}, 20);
