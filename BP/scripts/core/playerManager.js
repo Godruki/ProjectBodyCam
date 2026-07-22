ccf2a6e87c952b7d5aa5db96ae4686a997835922
Godruki <joaodinucci2010@gmail.com>
2026-07-22 05:56:13 -0300

    Update playerManager.js

--- BP/scripts/core/playerManager.js
+++ BP/scripts/core/playerManager.js
@@ -1,1 +1,25 @@
+/**
+ * ProjectBodyCam
+ * Player Manager
+ * Versión: 0.1.0
+ */
 
+import { world } from "@minecraft/server";
+import { createCamera } from "./cameraManager.js";
+
+world.afterEvents.playerSpawn.subscribe((event) => {
+
+    const player = event.player;
+
+    // Solo inicializamos la cámara la primera vez que aparece
+    if (event.initialSpawn) {
+
+        const camera = createCamera(player);
+
+        console.warn(
+            `[ProjectBodyCam] Cámara creada para ${player.name} (${camera.cameraId})`
+        );
+
+    }
+
+});
