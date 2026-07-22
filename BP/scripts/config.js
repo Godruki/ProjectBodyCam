24c978c3968e333d4491a90714594bd1543843e0
Godruki <joaodinucci2010@gmail.com>
2026-07-22 04:30:23 -0300

    Create config.js

---
+++ BP/scripts/config.js
@@ -0,0 +1,25 @@
+export const Config = {
+
+    Enabled: true,
+
+    RecordKey: "minecraft:spyglass",
+
+    ShowRecordingIcon: true,
+
+    ShowTimestamp: true,
+
+    ShowBattery: true,
+
+    ShowCoordinates: false,
+
+    BatteryMax: 100,
+
+    BatteryDrainPerMinute: 2,
+
+    BatteryRechargeOnSleep: true,
+
+    AutoStartRecording: false,
+
+    Version: "1.0.0"
+
+};
